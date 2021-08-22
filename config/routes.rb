3a71377803d017fe140fee4e6fcc6c3a0d510a0b
David Gregorato <hydrosnake@gmail.com>
2021-08-22 11:49:10 +0900

    Changed the grid layout.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'lists#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :lists, only: %w[index show new create] do
     resources :bookmarks, only: %w[new create]
